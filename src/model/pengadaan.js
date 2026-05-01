8f8b8c92bb669df5825232a90e830d8ca5beb20e
enzie <mulyaenziepratama@gmail.com>
2026-05-01 14:51:45 +0700

    feat: add getById function to pengadaan model

---
+++ src/model/pengadaan.js
@@ -0,0 +1,11 @@
+const pool = require('../../config/db');
+
+const pengadaanMod = {
+    getById: async(id) => {
+        const result = await pool.query(`SELECT * FROM pengadaan WHERE id = $1`, [id]);
+
+        return result.rows[0]; //result.rows[0] kalau hasilnya adalah satu objek, kalau array hanya return.rows
+    },
+};
+
+module.exports = pengadaanMod;
