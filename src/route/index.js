90e2e64195843d6556f0a725859f9b31c1019d9e
enzie <mulyaenziepratama@gmail.com>
2026-05-02 12:38:00 +0700

    fix: delete agentCont variable function

--- src/route/index.js
+++ src/route/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const dashboardController = require('../controller/dashboardcont');
 const pengadaanCont = require('../controller/pengadaancont');
 
-
 router.get('/', dashboardController.index);
 
 router.get('/pengadaan/:id', pengadaanCont.detail);
