ddad0b9c22e9504a2bacfdfa7fbb71cecd28b0cf
enzie <mulyaenziepratama@gmail.com>
2026-05-02 12:34:43 +0700

    fix: fixing typo on pengadaan route

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,10 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controller/dashboardcont');
 const pengadaanCont = require('../controller/pengadaancont');
-const agentCont = require('../controller/agentcont');
+
 
 router.get('/', dashboardController.index);
 
-router.get('/pengadaan/id', pengadaanCont.detail);
-
-router.post('/analisis/id', agentCont);
+router.get('/pengadaan/:id', pengadaanCont.detail);
 
 module.exports = router;
