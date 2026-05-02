05773a407e225c85f97df4362ebb646f9e687765
enzie <mulyaenziepratama@gmail.com>
2026-05-02 16:43:58 +0700

    feat: add analysis route POST /analisis/:id

--- src/route/index.js
+++ src/route/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const router = express.Router();
 const dashboardController = require('../controller/dashboardcont');
 const pengadaanCont = require('../controller/pengadaancont');
+const agentCont = require('../controller/agentcont');
 
 router.get('/', dashboardController.index);
 
 router.get('/pengadaan/:id', pengadaanCont.detail);
+
+router.post('/analisis/:id', agentCont.analyze);
 
 module.exports = router;
