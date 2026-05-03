168ddb71369d8a166ac61434e45158a196be370b
enzie <mulyaenziepratama@gmail.com>
2026-05-03 10:35:52 +0700

    fix: fixing typo

--- src/model/pengadaan.js
+++ src/model/pengadaan.js
@@ -4,7 +4,7 @@ const pengadaanMod = {
     getById: async(id) => {
         const result = await pool.query(`SELECT * FROM pengadaan WHERE id = $1`, [id]);
 
-        return result.rows[0]; //result.rows[0] kalau hasilnya adalah satu objek, kalau array hanya return.rows
+        return result.rows[0]; //result.rows[0] kalau hasilnya adalah satu objek, kalau array pakai return.rows
     },
 };
 
